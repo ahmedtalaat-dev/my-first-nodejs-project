48c95baa71fee2cc9dfc8f7554c002a20fe78742
Ahmed Talaat <ahmed.talaat.ali05@gmail.com>
2026-08-24 08:29:38 +0300

    feat: implement full CRUD API for courses

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ app.use("/api/courses", courseRoutes);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+const user = "ahmedtalaatali04_db_user";
+const password = "5Eh6TTXnqASdUtUY";
+const url = "mongodb+srv://ahmedtalaatali04_db_user:5Eh6TTXnqASdUtUY@learn-mongo.ir0ey0a.mongodb.net/?appName=learn-mongo";
